da03595f2e46c17caca390b717efe242d5267ec5
zuocheng <zuocheng@yidian-inc.com>
2020-04-19 13:04:59 +0800

    Initial commit

---
+++ src/cn/zlk/test/Test.java
@@ -0,0 +1,14 @@
+package cn.zlk.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Map<String,Double> map = new HashMap<>();
+        map.put("t1",1.2d);
+        map.put("t2",1.3d);
+        map.put("t3",1.4d);
+    }
+}
